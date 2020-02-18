aaf17b8cabd44edc9c844ceaa6c2b19bb1fd09a3
samisnotinsane <sameen11@hotmail.com>
2020-02-18 00:19:31 +0000

    Submitting shows confirmation and hides form

--- js/client.js
+++ js/client.js
@@ -1,1 +1,22 @@
-console.log("Hello world!")
+console.log('Hello world!')
+
+const form = document.querySelector('form');
+const sendConfirmation = document.querySelector('.sent-confirm');
+
+sendConfirmation.style.display = 'none';
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // prevent page auto-refresh
+    const formData = new FormData(form);
+    const email = formData.get('email');
+    const message = formData.get('message');
+
+    const msg = {
+        email,
+        message
+    };
+
+    form.style.display = 'none';
+    sendConfirmation.style.display = '';
+    console.log(msg);
+});
