5ce456ea3adfadbe535cbcc2ebaaf812db2ce91e
samisnotinsane <sameen11@hotmail.com>
2020-02-18 13:14:13 +0000

    Adding ability to send email with form

--- js/client.js
+++ js/client.js
@@ -1,26 +1,42 @@
-console.log('Hello world!')
+window.addEventListener("DOMContentLoaded", function () {
+    // get the form elements defined in form
+    const sendConfirmation = document.querySelector('.sent-confirm');
+    const form = document.querySelector('form');
 
-const form = document.querySelector('form');
-const sendConfirmation = document.querySelector('.sent-confirm');
-const sendBtn = document.querySelector('sendBtn');
-const email = document.querySelector('#emailInput');
+    // Success and Error functions for after the form is submitted
+    function success() {
+        form.reset();
+        sendConfirmation.style.display = '';
+        form.style.display = 'none';
+    }
 
-sendConfirmation.style.display = 'none';
-// send.disabled = true;
+    function error() {
+        sendConfirmation.style.display = '';
+        form.style.display = 'none';
+        sendConfirmation.innerHTML = '⚠️ Dang! Something must\'ve broken. ' +
+        'Please <a href="https://twitter.com/samisnotinsane7">Tweet me</a> so I can fix this issue ASAP.';
+    }
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault(); // prevent page auto-refresh
-    const formData = new FormData(form);
-    const email = formData.get('email');
-    const message = formData.get('message');
-
-    const msg = {
-        email,
-        message
-    };
-
-    email.disabled = false;
-    form.style.display = 'none';
-    sendConfirmation.style.display = '';
-    console.log(msg);
+    // handle the form submission event
+    form.addEventListener("submit", function (ev) {
+        ev.preventDefault();
+        const data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+    });
 });
+
+// helper function for sending an AJAX request
+function ajax(method, url, data, success, error) {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            success(xhr.response, xhr.responseType);
+        } else {
+            error(xhr.status, xhr.response, xhr.responseType);
+        }
+    };
+    xhr.send(data);
+}
