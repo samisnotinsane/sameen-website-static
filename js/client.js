bb27dde542fab6fae6a5448c00aca80aaf8ad5ff
samisnotinsane <sameen11@hotmail.com>
2020-02-18 01:03:05 +0000

    Disabling form inputs

--- js/client.js
+++ js/client.js
@@ -2,8 +2,11 @@ console.log('Hello world!')
 
 const form = document.querySelector('form');
 const sendConfirmation = document.querySelector('.sent-confirm');
+const sendBtn = document.querySelector('sendBtn');
+const email = document.querySelector('#emailInput');
 
 sendConfirmation.style.display = 'none';
+// send.disabled = true;
 
 form.addEventListener('submit', (event) => {
     event.preventDefault(); // prevent page auto-refresh
@@ -16,6 +19,7 @@ form.addEventListener('submit', (event) => {
         message
     };
 
+    email.disabled = false;
     form.style.display = 'none';
     sendConfirmation.style.display = '';
     console.log(msg);
